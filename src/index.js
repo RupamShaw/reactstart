d478f3356cee4c3c40be80a56633fc470dab6332
Rupam <rupam.shaw@gmail.com>
2017-10-09 09:13:05 +1100

    getInitialState without changing state of component

--- src/index.js
+++ src/index.js
@@ -12,18 +12,43 @@ alert('hi'+med );
 //         );
 //     }
 // });
+
+var person ={ pname: 'rupam' ,
+              age: 30,
+              sex:'f'
+            };
+
+function ListComponent(props) {
+ return (<div>
+          <h1>Hello {props.pers.pname},  {props.name}</h1>
+          <p> age : {props.pers.age}</p>
+          <p> todos :{props.todos}</p>
+        </div>
+ );
+}
+
 //Create a component  this is not ES6
-// var TodoComponent =  createReactClass({
-//     render: function(){
-//         return(
-//           <div>
-//             <p>hello from para</p>
-//             <h1>Hellooooo without ES6 feature</h1>
-//           </div>
-//         );
-//     }
-// });
-//ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+var TodoComponent =  createReactClass({
+  getInitialState: function(){
+      return {
+          todos: ['wash up', 'eat some cheese', 'take a nap']
+      }
+  }, //getInitialState
+  render: function(){
+      return(
+          <div id="todo-list">
+              <p>The busiest people have the most leisure...</p>
+              <ul>
+                  <li>{this.state.todos[0]}</li>
+                  <li>{this.state.todos[1]}</li>
+                  <li>{this.state.todos[2]}</li>
+              </ul>
+              <ListComponent pers ={person} todos={this.state.todos}/>
+          </div>
+      );
+  } //render
+});
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
 //ES6 feature is class
 // class TodoComponent  extends React.Component {
 //   render() {
@@ -37,16 +62,16 @@ const element = <h1>Hello, world!</h1>;
 
 ///Conceptually, components are like JavaScript functions. They accept arbitrary
 //inputs (called “props”) and return React elements describing what should appear on the screen.
-var person ={ pname: 'rupam' ,
-              age: 30,
-              sex:'f'
-            };
-function TodoComponent(props) {
- return (<div>
-          <h1>Hello {props.pers.pname},  {props.name}</h1>
-          <p> age : {props.pers.age}</p>
-        </div>
- );
-}
-
- ReactDOM.render(<TodoComponent name="from function" pers={person} />, document.getElementById('todo-wrapper'));
+// var person ={ pname: 'rupam' ,
+//               age: 30,
+//               sex:'f'
+//             };
+// function TodoComponent(props) {
+//  return (<div>
+//           <h1>Hello {props.pers.pname},  {props.name}</h1>
+//           <p> age : {props.pers.age}</p>
+//         </div>
+//  );
+// }
+//
+//  ReactDOM.render(<TodoComponent name="from function" pers={person} />, document.getElementById('todo-wrapper'));
