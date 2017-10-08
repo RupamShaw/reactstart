dd8ee76cbdd682bc6a1bcb8d75c942aca047c2d0
Rupam <rupam.shaw@gmail.com>
2017-10-08 18:57:52 +1100

    initial setup for react

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+module.exports = {
+
+    //define entry point
+    entry: './src/script-1.js',
+
+    //defin output point
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+
+     module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['react','es2015']
+                    }
+                  }
+
+            },
+            {
+              test    : /\.css$/,
+              use     : [
+                "style-loader",
+                "css-loader"
+
+              ]
+            }
+        ] //loaders
+    }
+
+
+};
