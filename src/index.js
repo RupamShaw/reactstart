abc735aba52b8a047b4944dea4d0afc584868975
Rupam <rupam.shaw@gmail.com>
2017-10-11 10:14:13 +1100

    routing in todo includes about

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
-
+import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 //module requires
 var TodoItem = require('./todoItem')
 
 var AddItem = require('./addItem');
 
+var About = require('./about');
+
 //CSS requires
 require('./css/index.css');
-
+const Header = () => (
+  <header>
+      <ul>
+        <li><Link to='/about'>About</Link></li>
+    </ul>
+  </header>
+)
 var TodoComponent =  createReactClass({
   getInitialState: function(){
       return {
@@ -23,20 +33,22 @@ var TodoComponent =  createReactClass({
     });
     return(
       <div id="todo-list">
+        <Link to='/about'>About</Link>
         <p>The busiest people have the most leisure...</p>
         <ul>{todos}</ul>
-<AddItem onAdd={this.onAdd} />
+        <AddItem onAdd={this.onAdd} />
       </div>
     );
   } ,//render
   //Custom functions
    onDelete: function(item){
-       var updatedTodos = this.state.todos.filter(function(val, index){
-           return item !== val;
-       });
+       var updatedTodos = this.state.todos.filter(
+                              function(val, index){
+                                return item !== val;
+                              });
        this.setState({
-         todos: updatedTodos
-       });
+                      todos: updatedTodos
+                    });
    },
 
     onAdd: function(item){
@@ -48,5 +60,19 @@ var TodoComponent =  createReactClass({
     }
 });
 
+//SETUP ROUTING
+var App = createReactClass({
+    render: function(){
+        return(
+            <BrowserRouter >
+            <main>
+                <Route exact path={"/"} component={TodoComponent}></Route>
+                <Route  path={"/about"} component={About}></Route>
+            </main >
+            </BrowserRouter>
+        );
+    }
+});
 
-ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+
+ReactDOM.render(<App />, document.getElementById('todo-wrapper'));
